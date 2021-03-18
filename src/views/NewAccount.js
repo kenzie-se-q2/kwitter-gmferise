4977c8ca39d4678c08c52ba0770a7298c4e6cf4f
Cedrick Randolph <naseerbiz@gmail.com>
2021-03-18 18:38:44 -0400

    signup page stlye is done

--- src/views/NewAccount.js
+++ src/views/NewAccount.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Button, Form } from 'semantic-ui-react'
+import "../assets/newaccount.css"
 
 const NewAccount = (props) => {
   return (
-  <Form>
-    <Form.Field>
-      <label>Username</label>
-      <input placeholder='Username' />
-    </Form.Field>
-    <Form.Field>
-      <label>Display Name</label>
-      <input placeholder='Display Name' />
-    </Form.Field>
-    <Form.Field>
-      <label>Password</label>
-      <input placeholder='Password' />
-    </Form.Field>
-   
-    <Button type='submit'>Submit</Button>
+    <div style={{height: 'inherit', width: 'inherit', backgroundColor: '#7a7474'}}>
+      <div id="newaccount-form">
+        <h1>Sign Up</h1>
+        <Form>
+          <Form.Field>
+            <label>Username</label>
+            <input placeholder='Username' />
+          </Form.Field>
+          <Form.Field>
+            <label>Display Name</label>
+            <input placeholder='Display Name' />
+          </Form.Field>
+          <Form.Field>
+            <label>Password</label>
+            <input placeholder='Password' />
+          </Form.Field>
+        
+          <Button type='submit'>Submit</Button>
 
-  </Form>
+        </Form>
+      </div>
+    </div>
 
 );
 }
