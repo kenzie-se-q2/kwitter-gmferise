06299aafec373be8e1cabf14d0af1b4492742632
siddiq_douglas <siddiq.douglas@outlook.com>
2021-03-18 11:24:04 -0400

    added

--- src/views/NewAccount.js
+++ src/views/NewAccount.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import 'semantic-ui-css/semantic.min.css'
 import { Button, Form } from 'semantic-ui-react'
 
 const NewAccount = (props) => {
+  return (
   <Form>
     <Form.Field>
       <label>Username</label>
       <input placeholder='Username' />
     </Form.Field>
     <Form.Field>
-      <label>Full name</label>
-      <input placeholder='Username' />
+      <label>Display Name</label>
+      <input placeholder='Display Name' />
     </Form.Field>
     <Form.Field>
       <label>Password</label>
@@ -20,30 +20,7 @@ const NewAccount = (props) => {
     <Button type='submit'>Submit</Button>
 
   </Form>
-  return (
-<div className= "New-Account"> 
-<h1>Creat New Account</h1>
 
-
-<form class="ui form">
-<div class="field">
-<label>Username</label>
-<input placeholder="Username"/>
-</div>
-
-<div class="field">
-<label>Password</label>
-<input placeholder="new Password"/>
-</div>
-
-
-
-<button type="submit" class="ui button">Submit</button>
-</form>
-
-
-</div>
 );
 }
 export default NewAccount
-  reactDom.render(<App />, document.getElementById("root"));
