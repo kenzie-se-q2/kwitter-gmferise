0b64cb7002d02f37719574b6cde597b010a0993f
siddiq_douglas <siddiq.douglas@outlook.com>
2021-03-23 13:22:34 -0400

    logout working in Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,8 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'semantic-ui-react';
 import '../assets/navigation.css';
+import { useStore } from '../store/store';
+import { logoutRequest } from '../apis/social';
 
 function Navigation (props) {
+  const user = useStore((state) => state.user);
+  const dispatch = useStore((state) => state.dispatch);
+
+  const logout = (e) => {
+    logoutRequest(user.token).then(() => dispatch({ type: 'LOGOUT' }));
+  };
+
 return(
     <div className="navBar">
       <div id="back-button">
@@ -11,7 +20,12 @@ return(
       </div>
       <div id="nav-link">
         <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
+
+        
+        
+        {user.token ? <a onClick={logout}> Logout </a> : <Link to="/login">Login </Link>}
+        
+
         <Link to="/signup">Create Account</Link>
       </div>
     </div>
