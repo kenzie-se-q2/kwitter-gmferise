26289cb0d2154783f4ca542636d318b234507ccf
Cedrick Randolph <naseerbiz@gmail.com>
2021-03-24 10:23:58 -0400

    edited notfound view

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../assets/notFound.css';
 
 function NotFound(props) {
-  return (
-    <>
+  return ( <><img src="https://cdn.dribbble.com/users/722246/screenshots/3066818/404-page.gif"/>  
       <p>Page not found for {props.location.pathname}</p>
       <Link to="/">Go Home</Link>
     </>
