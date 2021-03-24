626be4019f184261b8c6b3e53351e868a82744fb
Cedrick Randolph <naseerbiz@gmail.com>
2021-03-24 17:53:35 -0400

    styled the messageItem component

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Message, TextArea, Button, Icon, Label } from 'semantic-ui-react';
+import '../assets/messageItem.css'
 
 function Messages (props) {
   return(
-    <div className="messages">
-  <Message compact>
-    <div className=""><img /></div>
-    <div className=""><Message.Header>{props.displayName} <br /> @{props.username}</Message.Header></div>
-    <div className=""><p>{props.message}</p></div>
-    <Button as='div' labelPosition='right'>
-      <Button icon>
-        <Icon name='heart' />
-        Like
-      </Button>
-      <Label as='a' basic pointing='left'>
-        {props.likeCount}
-      </Label>
-    </Button>
-  </Message>
-  </div>
+    <Message compact id="message">
+        <div id="user-info">
+          <img className="user-image" src="https://via.placeholder.com/50" />
+          <div><Message.Header><span>{props.displayName}</span> <br /> <span>@{props.username}</span></Message.Header></div>
+        </div>
+        <div id="content"><p>{props.message}</p></div>
+        <Button as='div' id='like-button' labelPosition='right'>
+        <Button icon>
+          <Icon name='heart' />
+          Like
+        </Button>
+        <Label as='a' basic pointing='left'>
+          {props.likeCount}
+        </Label>
+        </Button>
+    </Message>
   );
 }
 
